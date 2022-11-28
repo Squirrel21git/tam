d0c9fa94bfecf520b606c7073634fd32494d4d56
Squirrel <squirrelgithub@gmail.com>
2022-11-28 08:10:08 +0100

    24-11

---
+++ commandsWPF/CustomCommands.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace commandsWPF
+{
+    internal class CustomCommands
+    {
+        public static readonly RoutedUICommand Exit = new RoutedUICommand(
+                "Exit from app",
+                "Exit",
+                typeof(CustomCommands),
+                new InputGestureCollection()
+                {
+                    new KeyGesture(Key.Q, ModifierKeys.Control)
+                }
+            );
+    }
+}
